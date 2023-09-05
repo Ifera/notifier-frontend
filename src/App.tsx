6b64c67f5ba946e8788d44eba599e43d3921cca8
Ifera <iferareaves@gmail.com>
2023-09-05 10:56:38 +0500

    populate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <Button variant='contained'>Hello world</Button>
     </>
   );
 }
