e6972a3fb1646e419d8d3b7bc66f086570145038
Ifera <iferareaves@gmail.com>
2023-09-05 17:54:04 +0500

    added placeholder grid for notification types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import './App.css';
 import Event from './components/event';
+import NotificationType from './components/notification-types';
 
 function App() {
   return (
     <>
       <Container>
         <Event />
+        <Box mt={2}>
+          <NotificationType />
+        </Box>
       </Container>
     </>
   );
