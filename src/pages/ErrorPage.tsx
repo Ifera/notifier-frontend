db6425a26890e129d07c45a9a60d63a6da16243f
abdullahqaisar <abdullahqaisarr@gmail.com>
2023-09-18 16:12:06 +0500

    add error page route

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+
+function ErrorPage() {
+  return (
+    <Box
+      sx={{
+        background: '#EBF5FF',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
+          paddingTop: '50vh',
+          color: '#001E3C',
+        }}
+      >
+        <b>404</b>
+        <span style={{ margin: '0 10px' }}>|</span> This Page could not be found
+      </Box>
+    </Box>
+  );
+}
+
+export default ErrorPage;
